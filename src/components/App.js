bcb06e07349e3592610d80614dc2bc8fe49c5955
Patri <patri@malvidopazo.es>
2022-01-18 11:03:45 +0100

    add last changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [student, setStudent] = useState([]); //para guardar los datos devueltos por al API
-
+const [newStudent, setNewStudent]= useState ({});
   useEffect(() => {
     callToApi().then((response) => {
       setStudent(response);
@@ -24,6 +24,25 @@ function App() {
       );
     });
   };
+//--------------------------------------------
+const handleInput=(ev)=> {
+  const inputValue = ev.currentTarget.value;
+  const inputName = ev.currentTarget.value;
+  setStudent ({...student, [inputName]:inputValue});
+
+};
+
+//--------------------------------------------------
+
+const handleSubmit = (ev)=> {
+  ev.preventDefault();
+  setStudent([...student,newStudent]);
+  setNewStudent ({
+    name:'',
+    counselor:'',
+    speciality:''
+  });
+};
 
   return (
     <div>
@@ -39,7 +58,27 @@ function App() {
 
         <tbody>{renderStudent()}</tbody>
       </table>
-
+      <section>
+        <h2>Añadir una adalaber</h2>
+        <form action="" onSubmit={handleSubmit} >
+          <label htmlFor="name">Nombre:
+            <input type="text" name="name" id="name"
+            onChange={handleInput}
+            value={student.name} />
+          </label>
+          <label htmlFor="counselor">Tutora:
+            <input type="text" name="counselor" id="counselor"
+           onChange={handleInput}
+            value={student.counselor} />
+          </label>
+          <label htmlFor="speciality">Especialidad:
+            <input type="text" name="speciality" id="speciality"
+            onChange={handleInput}
+            value={student.speciality} />
+          </label>
+          <button type="submit">Añadir una nueva adalaber</button>
+        </form>
+      </section>
     
     </div>
   );
