2ffc04b7ed4a8fc092289fd3baf09f655f9be6b2
Patri <patri@malvidopazo.es>
2022-01-24 16:15:55 +0100

    add bonus socialmedia

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ const callToApi = () => {
    
     return fetch('https://beta.adalab.es/pw-recursos/apis/adalabers-v1/promo-patata.json') 
       .then(response => response.json())
-      console.log(callToApi);
+     
   };
   
   export default callToApi;
